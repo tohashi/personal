b1001561d1fad9a0a2eef08b30cdf1740e96ef4a
t9m <dots64if@gmail.com>
2013-03-31 23:38:17 +0900

    Add views

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/form', routes.form);
+app.post('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
+var model = require('../model');
+var Post = model.Post;
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  Post.find({}, function(err, items) {
+    res.render('index', {title: 't9m workplace', items: items});
+  });
 };
+
+exports.form = function(req, res) {
+  res.render('form', {title: 'New Entry'});
+};
+
+exports.create = function(req, res) {
+  var newPost = new Post(req.body);
+  newPost.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect('back');
+    }else {
+      res.redirect('/');
+    }
+  });
+};
+
+exports.enchant = function(req, res) {
+  res.render('enchant', {title: 'enchant.js', js: '/javascripts/game.js'});
+}
