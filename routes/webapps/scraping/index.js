927aeab47771fa7f47536e28426d87f80cd838e8
t93 <dots64if@gmail.com>
2013-06-16 18:58:10 +0900

    Fix layout

--- routes/webapps/scraping/index.js
+++ routes/webapps/scraping/index.js
@@ -1,4 +1,7 @@
 exports.index = function(req, res) {
-  res.render('./webapps/scraping/index', {title: 'Scraping Test'});
+  res.render('./webapps/scraping/index', {
+      title: 'Scraping Test',
+      currentPath: '/webapps/scraping'
+  });
 };
 
